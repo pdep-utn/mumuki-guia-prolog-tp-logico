0e76373833c9697e25372d86f4fb074343a4866a
licfleischman.af@gmail.com <licfleischman.af@gmail.com>
2022-07-04 22:51:07 +0000

    Mumuki Export on 2022-07-04 22:51:07 +0000

--- 00002_Provincia picante/test.pl
+++ 00002_Provincia picante/test.pl
@@ -1,6 +1,6 @@
 test(es_picante_buenos_aires):-
     esPicante(buenosAires).
 test(es_picante_santa_fe, fail):-
-    esPicante(sanLuis).
-test(es_picante_san_luis, fail):-
     esPicante(santaFe).
+test(es_picante_san_luis, fail):-
+    esPicante(sanLuis).
